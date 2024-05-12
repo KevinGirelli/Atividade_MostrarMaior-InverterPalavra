4f5dd0254294fae13d8776799f7cdf5ec5990787
Kevin Girelli <kevingirelli.gg@gmail.com>
2024-05-12 14:37:37 -0300

    Add Home Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 import { MostrarMaiorComponent } from './components/mostrar-maior/mostrar-maior.component';
 import { InverterPalavraComponent } from './components/inverter-palavra/inverter-palavra.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
   {
     path: "maior",
     component: MostrarMaiorComponent
